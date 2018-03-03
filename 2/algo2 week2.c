548644c3748992d62452f4c853d64de973902222
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-03-03 13:17:09 +0200

    Update algo2 week2.c

--- 2/algo2 week2.c
+++ 2/algo2 week2.c
@@ -26,18 +26,18 @@ int x = 5;
 int *p;
 
 p = &x;
-printf("The address of %p\n", &x);       //x'in adresine eriþti
-printf("The value of p %p\n", p);        //p deðerinin x'in adresine eriþmesini istemiþtik
-printf("The value of x is %d\n",x);      //x'in deðeri
-printf("The value of x is %d accessed by dereferencing \n ",*p);       //p deðeri x'in adresine eriþmiþti.Xin adresine gittiðimizde x'in deðerini buluruz */
+printf("The address of %p\n", &x);       //x'in adresine erişti
+printf("The value of p %p\n", p);        //p değerinin x'in adresine erişmesini istemiştik
+printf("The value of x is %d\n",x);      //x'in değeri
+printf("The value of x is %d accessed by dereferencing \n ",*p);       //p değeri x'in adresine erişmişti.Xin adresine gittiğimizde x'in değerini buluruz */
 
 /* float a,b,r;
 float *pa=&a, *pb=&b;
 float *pr=&r;
-scanf(" %f",pa);               //scanf'in içine bi þey yazýlmaz!
+scanf(" %f",pa);               //scanf'in içine bi şey yazılmaz!
 scanf(" %f",pb);
 
-*pr=*pa+*pb;                   //a ve b deðerlerinin toplamýdýr.
+*pr=*pa+*pb;                   //a ve b değerlerinin toplamıdır.
 
 printf("%f\n",*pr);
 printf("%f\n",r);    */
@@ -50,8 +50,8 @@ double *pd1,*pd2;
 printf(" c is %c\n d is %f\n",c,d);
 printf(" pc is %c\n",*pc);
 pd1=&d;                 
-*pc='B';               //Artýk c gördüðümüz yerlere B yazýcaz.
-pd2=pd1;               //pd2 d'nin adresine eþit oldu.
+*pc='B';               //Artık c gördüğümüz yerlere B yazıcaz.
+pd2=pd1;               //pd2 d'nin adresine eşit oldu.
 *pd1=*pd2*0.2;         //d=5.43*0.2
 printf(" After \n");
 printf(" c is %c\n d is %f\n *pd1 is %f\n *pd2 is %f\n",c,d,*pd1,*pd2);
@@ -74,7 +74,7 @@ p=&a;
 printf("%d %d %d \n",a,b,c);
 printf("%d %d %d \n",*p,*q,*r); */
 
- void exchange(int *x,int *y)
+ /* void exchange(int *x,int *y)
 {
 	int temp;
 	temp=*x;
@@ -91,11 +91,46 @@ int main()
 	int a=5, b=7;
 	exchange(&a,&b);
 	printf("a=%d b=%d\n",a,b);
-	return 0;
+	return 0;*/
 	
+	
+	int main()
+	{
+	
+	
+	int buffer[3]={3,4,5};
+	int *pbuffer1, *pbuffer2, *pbuffer3, *pbuffer4, *pbuffer5;
+	
+	pbuffer3=buffer;
+	pbuffer4=&buffer[0];
+	pbuffer1=buffer;
+	pbuffer2=buffer+2;
+	pbuffer5=&buffer[2];
+	
+	
+	printf("buffer[0] is %d\t buffer[2] is %d\n" ,buffer[0], buffer[2]);
+	printf("Using pointer as an array name pbuffer1[1]=%d\n" ,pbuffer1[1]);
+	printf("Using array namae as pointer *(buffer+2)=%d\n" ,*(buffer+2));
+	printf("pbuffer is %d\t *pbuffer2 is %d\n\n\n" ,*pbuffer1, *pbuffer2);
+	printf("pbuffer4 is %d\n",pbuffer4);
+	printf("pbuffer5 is %d\n",pbuffer5);
+	
+	
+	
+return 0;	
 }
 
+
                                            
+
+
+
+
+
+
+
+
+                   
 
 
 
