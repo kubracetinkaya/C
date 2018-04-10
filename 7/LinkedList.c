352a15804f00ac18bbba9abba1078b2263065fdb
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-04-10 23:56:41 +0200

    Rename LinkedList.cpp to 7/LinkedList.c

--- 7/LinkedList.c
+++ 7/LinkedList.c
@@ -19,28 +19,28 @@ void yazdir(node* r )
 
 int main()
 {
-	node *root;                                    //root node* tipinde bir pointer.kutu olusturmak gibi düsün
+	node *root;                                    //root node* tipinde bir pointer.kutu olusturmak gibi dÃ¼sÃ¼n
 	root=(node*)malloc(sizeof(node));              
-	root->x=10;                                    //ilk kutunun datasýna 10 yaz
-	root->next=(node*)malloc(sizeof(node));        //deger atamadan önce hafýzada yer ayýrmayý unutma!
+	root->x=10;                                    //ilk kutunun datasÄ±na 10 yaz
+	root->next=(node*)malloc(sizeof(node));        //deger atamadan Ã¶nce hafÄ±zada yer ayÄ±rmayÄ± unutma!
 	root->next->x=20;                              
 	root->next->next=(node*)malloc(sizeof(node));
 	root->next->next->x=30;
-	root->next->next->next=NULL;                  //linkedlistin sonuncu elemanýnýn null olmasýný istiyoruz cünkü bellekte o alan boþ olmayabilir.
-	node *iter;                                  //nextleri cok kullanmamak icin baþka bir pointer kullandýk.iter kutular arasýnda dolansýn diye.
+	root->next->next->next=NULL;                  //linkedlistin sonuncu elemanÄ±nÄ±n null olmasÄ±nÄ± istiyoruz cÃ¼nkÃ¼ bellekte o alan boÅŸ olmayabilir.
+	node *iter;                                  //nextleri cok kullanmamak icin baÅŸka bir pointer kullandÄ±k.iter kutular arasÄ±nda dolansÄ±n diye.
 	iter=root;                                   
     printf("%d\n",iter->x);
     iter=iter->next;
     printf("%d\n",iter->x);
-    iter=root;                                //iteri ilk elemana aldýk
+    iter=root;                                //iteri ilk elemana aldÄ±k
     int i=0;                                  
    /* while(iter!=NULL)                         
     {
         i++;                                                      
-    	printf("%d inci eleman: %d\n",i,iter->x);             //linkedlist elemanlarýný sýrayla yazdýrdým.
+    	printf("%d inci eleman: %d\n",i,iter->x);             //linkedlist elemanlarÄ±nÄ± sÄ±rayla yazdÄ±rdÄ±m.
     	iter=iter->next;
 	}*/
-	  while(iter->next!=NULL)                      //linked listin sonuna eleman eklemek istiyorum. Ýterden sonrasi bos olmasin dedim
+	  while(iter->next!=NULL)                      //linked listin sonuna eleman eklemek istiyorum. Ä°terden sonrasi bos olmasin dedim
 	{ 
 	    i++;                                                    
     	printf("%d inci eleman: %d\n",i,iter->x);            
@@ -50,7 +50,7 @@ int main()
 	{
 		iter->next=(node*)malloc(sizeof(node));
 		iter=iter->next;
-		iter->x=i*10;                                     //sýrayla 0 10 20 30 40 yazýcak kutularda
+		iter->x=i*10;                                     //sÄ±rayla 0 10 20 30 40 yazÄ±cak kutularda
 		iter->next=NULL;
 	}
 	yazdir(root);
