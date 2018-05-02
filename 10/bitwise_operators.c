41b9c432a8c11f1db88a0eab34caba6c9627e32c
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-05-02 22:48:57 +0200

    Rename bitwise_operators.cpp to 10/bitwise_operators.c

--- 10/bitwise_operators.c
+++ 10/bitwise_operators.c
@@ -5,7 +5,7 @@ int main()
 	/*
 	int x=4;
 	printf("%d\n",x);
-	printf("%d\n",x<<=1);         //<< sola kaydýr demek yani 2 ile çarpmak demek.= anlamý da deðeri kaydýrýlan deðer olsun demek.
+	printf("%d\n",x<<=1);         //<< sola kaydÄ±r demek yani 2 ile Ã§arpmak demek.= anlamÄ± da deÄŸeri kaydÄ±rÄ±lan deÄŸer olsun demek.
 	printf("%d\n",x);
 	printf("%d\n",x>>1);
 	
@@ -14,10 +14,10 @@ int main()
 	int x=3;
 	int y=8;
 	int z=5;
-	printf("%d\n",x&y);   //bit bit &leme iþlemi yapýyor
+	printf("%d\n",x&y);   //bit bit &leme iÅŸlemi yapÄ±yor
 	printf("%d\n",x|y);
 	printf("%d\n",x^y);
-	printf("%d\n",x^y|z);       //iþlem önceliði ^ & |
+	printf("%d\n",x^y|z);       //iÅŸlem Ã¶nceliÄŸi ^ & |
 	printf("%d\n",x|y&z);
 	printf("%d\n",x^y&z|x);
 	
